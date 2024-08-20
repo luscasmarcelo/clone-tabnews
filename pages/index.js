03f5601872a7ed3335a8cca8b5fb50c218794cb4
Lucas Marcelo Sampaio Santos <149593118+luscasmarcelo@users.noreply.github.com>
2024-08-20 20:31:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return <h1>meu primeiro projeto sério
-            vasco da gama
-    </h1>
+  return <h1>meu primeiro projeto sério vasco da gama</h1>;
 }
 
 export default Home;
